7efd057acb12bb0229b271ff69bb43ed86bdccc8
amitrani6 <amitrani1206@gmail.com>
2020-09-12 23:39:03 -0400

    updated api

--- fmppassword.py
+++
@@ -1,5 +0,0 @@
-#This file contains the Access Key for the "Financial Modeling Prep" API
-
-client_id = 'amitrani@bu.edu' #Your client ID goes here (as a string)
-#api_key = 'P5cux20CPLgjDwG7Dlrh70WkbFte3CTQ' #Your api key goes here (as a string)
-api_key = '1fc7b080cf538b0508d969fcc4bb11f2'
